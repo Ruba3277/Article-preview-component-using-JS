ddbd2a45b0698d91ef034faa18a21a6dcc3c17b7
Ruba3277 <rubabbalraj@gmail.com>
2026-06-09 13:26:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,30 @@
-const share=document.querySelector(".share");
-const sharePanel=document.querySelector(".share-panel");
-const contactDetails=document.querySelector(".contact-details");
-const sharePanelIcon=document.querySelector(".share-panel-icon");
-const productDetails=document.querySelector(".product-details");
-function isDesktop() {
-    return window.innerWidth >= 628; 
-}
-share.addEventListener("click",()=>{
-    if(isDesktop()){
-        sharePanel.classList.toggle("hidden");
-        contactDetails.classList.remove("hidden");
-        productDetails.classList.remove("share-active");
+const sharePanel = document.querySelector(".share-panel");
+const contactDetails = document.querySelector(".contact-details");
+const shareIcon = document.querySelector(".share-icon");
+const productDetails = document.querySelector(".product-details");
+const sharePanelIcon = document.querySelector(".share-panel-icon");
 
-    }
-    else{
-        sharePanel.classList.toggle("hidden");
-        contactDetails.classList.toggle("hidden");
-        productDetails.classList.toggle("share-active");
-    }
-       
-});
-sharePanelIcon.addEventListener("click",()=>{
+function isDesktop() {
+  return window.innerWidth >= 628;
+}
+
+shareIcon.addEventListener("click", () => {
+    
+  if (isDesktop()) {
     
     sharePanel.classList.toggle("hidden");
-    contactDetails.classList.remove("hidden");
-    productDetails.classList.toggle("share-active");
-   
+    shareIcon.classList.toggle("active");
     
+  } else {
+    sharePanel.classList.toggle("hidden");
+    contactDetails.classList.toggle("hidden");
+    productDetails.classList.toggle("share-active");
+  }
 });
 
-
+sharePanelIcon.addEventListener("click", () => {
+  sharePanel.classList.toggle("hidden");
+  contactDetails.classList.remove("hidden");
+  productDetails.classList.toggle("share-active");
+  shareIcon.classList.remove("active"); 
+});
